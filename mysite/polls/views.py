cdb78f44041b6aa87ab1d19b008f037dae8f1640
Maestro <buseigin2711@gmail.com>
2024-04-15 11:36:40 +0300

    edit_django_project

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
-from django.template import loader
 from django.views import generic
 from .models import Question, Choice
 from django.utils import timezone
@@ -12,9 +10,11 @@ from django.utils import timezone
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
+
     def get_queryset(self):
         """Return the five published questions."""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
@@ -22,12 +22,15 @@ class DetailView(generic.DetailView):
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
